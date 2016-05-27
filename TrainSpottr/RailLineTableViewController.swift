f795b623fbc630a0f697707029cbaa046bff4208
Will Williams <wdw5000@gmail.com>
2016-05-26 21:59:11 -0400

    setup dummy tableView cells

--- TrainSpottr/RailLineTableViewController.swift
+++ TrainSpottr/RailLineTableViewController.swift
@@ -10,10 +10,28 @@ import UIKit
 
 class RailLineTableViewController: UITableViewController {
     
+    private let cellId = "cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupView()
+    }
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellId, forIndexPath: indexPath)
+        
+        cell.textLabel?.text = "\(indexPath.row)"
+        
+        return cell
     }
     
     func setupView() {
